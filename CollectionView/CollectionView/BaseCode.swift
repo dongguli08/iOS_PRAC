09cdd00be2cdd5fa35e27f9d89c53adf16358b00
donggun <hugh3232@naver.com>
2025-07-03 12:17:38 +0900

    refactor:: 주석추가

--- CollectionView/CollectionView/BaseCode.swift
+++ CollectionView/CollectionView/BaseCode.swift
@@ -4,13 +4,12 @@ import UIKit
 
 
 public class BaseViewController: UIViewController {
-    func addSubview() { }
+    func addSubview() { } // 빈 함수
     func layout() { }
     
     override public func viewDidLoad() {
-        super.viewDidLoad()
-    
-        addSubview()
+        super.viewDidLoad() //상위 클래스 초기화 완료, 정상 동작
+        addSubview() // 호출해야 하위 클래스 구현 동작 실행
         layout()
     }
 }
